c74c41ae6dc6378046b875aee85ac1a6139a9ee8
Wzysaber <1149634047@qq.com>
2019-11-07 19:39:48 +0800

    learn1

---
+++ test.py
@@ -0,0 +1,22 @@
+import turtle
+import time
+import math
+
+def polypon(n): 
+    for i in range(n):
+        pen.fd(50)
+        pen.left(360/n)
+    
+def arc(p,r,angle):
+    L = 2 * math.pi * r *angle/360
+    n = int(L / 3) + 1
+    length = L / n
+
+    
+if __name__ == '__main__':
+    pen = turtle.Turtle()
+    
+
+    turtle.exitonclick()
+
+
